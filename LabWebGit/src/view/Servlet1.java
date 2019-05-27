92a1b042ed94d2ba1b98eeaf55f6824e991d6292
詹岷錠 <iii.edu@www>
2019-05-27 12:11:10 +0800

    第二次 Commit

--- LabWebGit/src/view/Servlet1.java
+++ LabWebGit/src/view/Servlet1.java
@@ -16,7 +16,10 @@ public class Servlet1 extends HttpServlet {
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("<h3>Servlet1:"+time+"</h3>");
+		out.println("<h3>Servlet1:"+time+"</h3>");
 		out.close();
+		
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
